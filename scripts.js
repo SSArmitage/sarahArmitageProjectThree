62b010231c8467f2775747f5386b44267e798d36
Sarah Armitage <saraharmitage314@gmail.com>
2019-11-17 22:24:48 -0500

    added favicon, fixed prize alert background image

--- scripts.js
+++ scripts.js
@@ -15,21 +15,9 @@
 // ***stretch goals*** 
 // => ☑️coin cursor
 // => animating scratch 
-// => additional games keeps track of accumulated prize
-// => add additional game to card: three boxes where the user scratches and has to find 3 identical symbol
-// => when prize amount is displayed to user, have coins fall across screen
-
-// *****to-do******
-// => cache selectors
-// => favicon
-// => accessible
-
-// ******design******
-// => copy actual lottery tickets
-// => bright colors
-// => cool font for name
-// => remove submit button when win/non-win alert pops up
-// => larger screens: keep "ticket" size w/ grey background
+// => additional games & keeps track of accumulated prize
+// => add additional bonus game to card: three boxes where the user scratches and has to find 3 identical symbol
+// => when prize amount is displayed to user, have coins fall top->bottom accross the screen
 
 
 // -------------------------
@@ -59,32 +47,6 @@ scratchCardApp.prizeAmountArray = [];
 scratchCardApp.gamePrizeAmount = 0;
 scratchCardApp.finalGamePrizeAmount;
 
-// hamburger menu (contains game instructions)
-// scratchCardApp.displayHamburger = function() {
-//     // $('header .flexContainer').append(`
-//     //     <i class="fa fa-bars"></i>
-//     // `)
-//     // $('main').append(`
-//     //     <div class=hamMenu>
-//     //         <h2>How to Play</h2>
-//     //     </div>
-//     // `);
-
-// }
-
-// cover scratch squares
-// $('#elem').wScratchPad({
-//     size: 5,          // The size of the brush/scratch.
-//     bg: ,  // Background (image path or hex color).
-//     fg: '#6699ff',  // Foreground (image path or hex color).
-//     realtime: true,       // Calculates percentage in realitime.
-//     scratchDown: null,       // Set scratchDown callback.
-//     scratchUp: null,       // Set scratchUp callback.
-//     scratchMove: null,       // Set scratcMove callback.
-//     cursor: 'crosshair' // Set cursor.
-// });
-
-
 // generate random numbers between 1-20 (number on scratch boxes)
 scratchCardApp.generateRandomNumber = function() {
     return Math.floor(Math.random() * 20) + 1;
@@ -95,17 +57,13 @@ scratchCardApp.generateRandomNumber2 = function () {
     return Math.floor(Math.random() * 8) + 1;
 }
 
-// assign random numbers/display in the scratch boxes
-// need to make it so that LUCKY NUMBERS c/n be the same
+// assign random numbers & display in the scratch boxes
 scratchCardApp.assignRandomNumber = function() {
     // assign LUCKY NUMBERS
-    // let luckyNumbersArray = [];
     for (let i = 1; i <= 2; i++) {
         let randomNumber = scratchCardApp.generateRandomNumber();
-        // checks to see if the generated random number is already in the array (ensures that the LUCKY numbers are always differnt) 
+        // check to see if the generated random number is already in the array, and if so, decrement i by 1 (ensures that the two LUCKY numbers are always different) 
         if (scratchCardApp.luckyNumbersArray.includes(randomNumber)) {
-            // console.log(luckyNumbersArray);
-            // console.log(`I am a duplicate: ${randomNumber}`);
             i--; 
         }else {
             scratchCardApp.luckyNumbersArray.push(randomNumber);
@@ -122,13 +80,11 @@ scratchCardApp.assignRandomNumber = function() {
         let randomNumber = scratchCardApp.generateRandomNumber();
          // add random numbers to the yourNumbersArray (array will be used to compare YOUR NUMBERS and LUCKY NUMBERS)
         scratchCardApp.yourNumbersArray.push(randomNumber);
-
         // pick random prize to go in scratch box
         let randomPrizeIndex = scratchCardApp.generateRandomNumber2();
         let randomPrize = scratchCardApp.prizeArray[randomPrizeIndex];
         // add random prize to the prizeAmountArray (array will be used to tally prize for matching numbers)
         scratchCardApp.prizeAmountArray.push(randomPrize);
-
         // display the random number and prize in the scratch box
         // also display a cover for the scratch box
         $(`.box${i}`).html(`
@@ -141,57 +97,49 @@ scratchCardApp.assignRandomNumber = function() {
 
 // determining the prizeAmountWon for this game
 scratchCardApp.prizeAmountWon = function () {
-   
-    // map over the prizeAmountArray (all the randomly chosen prize amounts for this instance of the game) => remove the $ from the prize amounts, convernt the string into a number, and then return in a new array newPrizeArray
+    // map over the prizeAmountArray (all the randomly chosen prize amounts for this instance of the game) => remove the $ from the prize amounts, convert the string into a number, and then return in a new array newPrizeArray
     // doing this b/c need to add the prize amounts together (need to be numbers to do this)
     let numberPrizeArray = scratchCardApp.prizeAmountArray.map(function(item) {
         return parseInt(item.slice(1));
-        
-    })
-    console.log(numberPrizeArray);
-    
+    });
     // check to see if any of the YOUR NUMBERS matches with either of the LUCKY NUMBERS 
     // loop through the items in yourNumbersArray (the 6 randomly chosen numbers displayed in YOUR NUMBERS scratch boxes)
     for (let i = 0; i < 6; i++) {
         // if the number at the ith position of yourNumbersArray is equal to the first LUCKY NUMBER or if it is equal to the second LUCKY NUMBER
         if (scratchCardApp.yourNumbersArray[i] === scratchCardApp.luckyNumbersArray[0] || scratchCardApp.yourNumbersArray[i] === scratchCardApp.luckyNumbersArray[1]) {
-            
             // prizeAmount started at 0, add the matching numbers' prize value to the prizeAmount variable (able to do this b/c the yourNumbersArray[i] and numberPrizeArray[i] correspond to the same ith scratch box)
             scratchCardApp.gamePrizeAmount += numberPrizeArray[i];
-            console.log(numberPrizeArray[i]);
-            console.log(scratchCardApp.gamePrizeAmount); 
-
-            // NEED TO SET gamePrizeAmount BACK TO 0 ON RESET
         }
     }
     // convert prizeAmount back to a string and add "$" to the front
     scratchCardApp.finalGamePrizeAmount = `$${scratchCardApp.gamePrizeAmount.toString()}`;
-    console.log(scratchCardApp.finalGamePrizeAmount);
 }
 
 // display prize screen to alert user of the amount won during this game
 scratchCardApp.displayPrizeScreen = function() {
+    // winner card
     if (scratchCardApp.finalGamePrizeAmount !== '$0') {
-        console.log("you win");
-        console.log(scratchCardApp.finalGamePrizeAmount);
-        
         $('main .flexContainer').append(`
         <div class="prizeAlertContainer">
             <div class=prizeAlert>
                 <p>Winner!</p>
                 <p>${scratchCardApp.finalGamePrizeAmount}</p>
+                <div id="starBurst" class="imageContainer">
+                    <img src="./assets/starburst.png">
+                </div>
                 <button class="reset outline">New Card</button>
             </div>
         </div>    
         `); 
+    // non-winner card
     } else {
-        console.log(scratchCardApp.finalGamePrizeAmount);
-        console.log(`you lose`);
-
         $('main .flexContainer').append(`
         <div class="prizeAlertContainer">
             <div class=prizeAlert>
                 <p>Non-winner</p>
+                <div id="starBurst" class="imageContainer">
+                    <img src="./assets/starburst.png">
+                </div>
                 <button class="reset outline">New Card</button>
             </div>
         </div>    
@@ -203,25 +151,17 @@ scratchCardApp.submitButtonToggle = function() {
     $('.submit').toggleClass('hide');
 }
 
-// contains all the events to listen for
+// function that contains all the events to listen for
 scratchCardApp.events = function() {
     //event listeners here
 
     // click on HAMBURGER MENU
-
-    // $('.flexContainer').on('click', 'i', function() {
-    //     console.log("I clicked the hamburger");
-    //     $('.hamMenu').toggleClass('openMenu');
-    // });
-
     $('.fa-bars').on('click', function() {
-        console.log("I clicked the hamburger");
         $('.hamMenu').toggleClass('openMenu');
     })
 
     // click on SCRATCH BOX
     $('.numBox').on('click','.scratchBoxCover', function() {
-        console.log(`Scratching...`);
         $(this).addClass('scratched');
     })
 
@@ -236,8 +176,6 @@ scratchCardApp.events = function() {
     // click on RESET BUTTON
     // resets the card
     $('main .flexContainer').on('click', '.reset', function() {
-        console.log('reset button clicked!');
-
         // remove the prize alert
         $('.prizeAlertContainer').remove(); 
         // reset variables
@@ -250,25 +188,19 @@ scratchCardApp.events = function() {
         $('.scratchBoxCover').removeClass('scratched');
         // call the fxn to assign the random numbers to the scratch boxes
         scratchCardApp.assignRandomNumber();
-
-        // 
-        // $('.submit').removeClass('.displayNone');
+        // make the submit button visible again
         scratchCardApp.submitButtonToggle();
     })
-
 }
 
 // INIT FXN
 // contains fxns ready to be initialized at runtime
 scratchCardApp.init = function() {
     scratchCardApp.events();
-    // scratchCardApp.displayHamburger();
     scratchCardApp.assignRandomNumber();
-    console.log("document is initalized");
 }
 
 // DOCUMENT READY
 $(document).ready(function () {
     scratchCardApp.init();
-    console.log("document is ready!");
 });
